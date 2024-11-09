81be7012c7eb015e604fcb8d2d98e21b86d548aa
Me-Phew <basmateusz@wp.pl>
2024-11-09 18:48:32 +0100

    docs: add project icon and logo

--- docs/doxygen/html/search/all_2.js
+++ docs/doxygen/html/search/all_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['list_0',['List',['../class_list.html',1,'']]],
-  ['listy_20dwukierunkowej_1',['Algorytm listy dwukierunkowej',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['licencja_20a_0',['&lt;a name=&quot;license&quot;&gt;⚖️ Licencja&lt;/a&gt;',['../md__r_e_a_d_m_e.html#autotoc_md4',1,'']]],
+  ['license_20⚖️_20licencja_20a_1',['&lt;a name=&quot;license&quot;&gt;⚖️ Licencja&lt;/a&gt;',['../md__r_e_a_d_m_e.html#autotoc_md4',1,'']]],
+  ['list_2',['List',['../class_list.html',1,'']]]
 ];
---
+++ docs/doxygen/html/search/all_c.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['🧩_20funkcjonalność_20a_0',['&lt;a name=&quot;features&quot;&gt;🧩 Funkcjonalność&lt;/a&gt;',['../md__r_e_a_d_m_e.html#autotoc_md3',1,'']]]
+];
